7f75a329da138e7a77d661bee4901b4a622bf6da
Christian Rice <ctr22b@fsu.edu>
2023-09-19 22:49:18 -0400

    First Edit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let x = 200;
+let speed = 5;
+let diameter = 40;
+
 function setup() {
   createCanvas(400, 400);
+  noLoop();
 }
 
 function draw() {
-  background(125);
+  background(255,125,0);
+  ellipse( x, height/2, diameter);
+  if (x >= width || x<=0) {
+    speed = speed * -1;
+  }
+  x += speed;
 }
+
+   function mouseReleased() {
+    loop();
+  }
+  
+function mousePressed() {
+    noLoop();
+
+    
+
+  }
+
+ 
